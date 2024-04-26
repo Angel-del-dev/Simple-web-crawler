19fdeaa366662c111a746913e20f638c0c0e574c
AngelNovo <angelnovo15@gmail.com>
2024-04-26 23:37:22 +0200

    End project

--- main.v
+++ main.v
@@ -7,7 +7,7 @@ import time
 
 fn parse( mut url_list []string, url string)  {
 	contents := http.get(url) or {
-		panic('Error finding the url')
+		panic('Error finding the url <<$url>>')
 	}
 
 	body := html.parse(contents.str())
@@ -54,11 +54,24 @@ fn check_if_valid_link(link string) bool {
 	mut link_arr := link.split('/')
 
 	end := link_arr.pop()
-	return !end.contains('.')
+	return !end.contains('.') && link.split('')[0] != '.'
 }
 
 fn seconds_to_nano_seconds(seconds f32) f32 {
 	return seconds * 1_000_000
+}
+
+fn get_domain_from_url(url string) string {
+
+	if url.split('')[0] == '/' { return '' }
+	mut domain := ''
+	mut new_url := url.split('://')
+
+	if new_url.len > 1 {
+		rest := new_url[1].split('/')
+		domain = new_url[0]+'://'+rest[0]
+	}
+	return domain
 }
 
 fn main() {
@@ -68,18 +81,34 @@ fn main() {
 
 	unsafe {
 		mut links := []string{}
+		mut domains := []string{}
+
+		domains << get_domain_from_url(initial_url)
 
 		parse(mut links, initial_url)
 
 		mut read_links := []string{}
-
 		for {
 			if links.len == 0 { break }
 			mut link := links[0].str()
-			if link.split('')[0] == '/' { link = '$initial_url$link' }
+
+			new_domain :=  get_domain_from_url(link)
+
+			if new_domain == '' { domains << domains.last() }
+			else {
+				domains << new_domain
+			}
+
+			current_domain := domains.last()
+
+			if link.split('')[0] == '/' {
+				link = '$current_domain$link'
+			}
+			domains.pop()
 
 			if read_links.contains(link) {
 				links.delete(0)
+				time.sleep(nanoseconds)
 				continue
 			}
 			links.delete(0)
--- readme.md
+++ readme.md
@@ -4,10 +4,7 @@
 * Algorithm is less aggressive
   * Added a 0.2s delay between requests
 
-## Todo:
-* Configurable
-  * Allow if the crawler can leave the domain
-  * Allow max petition count
-  * Delay between requests
-  * What information to gather
-  * What to do with the given information
+## Execute:
+```batch
+program.exe http://url
+```
